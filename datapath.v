7bccf2626eee7c32ed0b408319dfbf942f7cedd4
Manish Garg <manishgargpro@gmail.com>
2019-11-26 20:45:15 -0500

    fixed ios in 'datapath' and added formatted version of 'register_file'

--- datapath.v
+++ datapath.v
@@ -19,49 +19,15 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module datapath(
-    input clk_main,
-    input reset,
-    input [2:0] DR,
-    input [2:0] SA,
-    input [2:0] SB,
-    input [3:0] AX,
-    input [3:0] BX,
-    input [3:0] DX,
-    input [3:0] FS,
-    input MB,
-    input MM,
-    input MD,
-	 input MW,
-    input BusA,
-    input [nbit-1:0] DataOut,
-    output V,
-    output C,
-    output N,
-    output Z
+    input clk_main, reset,
+    input [2:0] DR, SA, SB,
+    input [3:0] AX, BX, DX, FS,
+    input MB, MM, MD, MW,
+    output [nbit-1:0] BusA, DataOut,
+    output V, C, N, Z
     );
 	 
 	 parameter nbit = 16;
-	 
-	 MUX_2to1 MUXB (
-    .In0(In0), 
-    .In1(In1), 
-    .S(MB), 
-    .Out(Out)
-    );
-
-	MUX_2to1 MUXM (
-    .In0(In0), 
-    .In1(In1), 
-    .S(MM), 
-    .Out(Out)
-    );
-
-	MUX_2to1 MUXD (
-    .In0(In0), 
-    .In1(In1), 
-    .S(MD), 
-    .Out(Out)
-    );
 
 
 endmodule
