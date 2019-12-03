d7a3eb6a45982021c0053591aa9a3f3262b4ca7b
m-we <mwea@protonmail.com>
2019-12-03 10:41:11 -0500

    Fixed ALU

--- ALU.v
+++ ALU.v
@@ -9,23 +9,23 @@ module ALU(
 
 always @(*) begin
 	case (FS)
-		4'b0000: num_out <= A + B;
-		4'b0001: num_out <= A - B;
-		4'b0010: num_out <= A && B;
-		4'b0011: num_out <= A || B;
-		4'b0100: num_out <= A ^ B;
-		4'b0101: num_out <= ~A;
-		4'b0110: num_out <= A<<0;
-		4'b0111: num_out <= A>>0;
-		4'b1000: num_out <= B;
-		4'b1001: num_out <= num_out;
-		4'b1010: num_out <= num_out;
-		4'b1011: num_out <= A;
-		4'b1100: num_out <= A;
-		4'b1101: num_out <= num_out;
-		4'b1110: num_out <= num_out;
-		4'b1111: num_out <= A;
-		default: num_out <= num_out;
+		4'b0000: begin num_out <= A + B; end
+		4'b0001: begin num_out <= A - B; end
+		4'b0010: begin num_out <= A & B; end
+		4'b0011: begin num_out <= A | B; end
+		4'b0100: begin num_out <= A ^ B; end
+		4'b0101: begin num_out <= ~A; end
+		4'b0110: begin num_out <= A << 1; end
+		4'b0111: begin num_out <= A >> 1; end
+		4'b1000: begin num_out <= B; end
+		4'b1001: begin num_out <= num_out; end
+		4'b1010: begin num_out <= num_out; end
+		4'b1011: begin num_out <= A; end
+		4'b1100: begin num_out <= A; end
+		4'b1101: begin num_out <= num_out; end
+		4'b1110: begin num_out <= num_out; end
+		4'b1111: begin num_out <= A; end
+		default: begin num_out <= num_out; end
 	endcase
 
 	if (num_out == 16'b0)
